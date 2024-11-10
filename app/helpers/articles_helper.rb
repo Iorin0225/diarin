7004b1a8b0b01ec5d9e30e1522acbcac06f80ac0
Iori OSADA <iori.osada@gmail.com>
2024-11-10 11:45:30 +0900

    feat: support embedding instagram

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,6 +1,6 @@
 module ArticlesHelper
   def body_with_pre_wrap(article)
-    safe_html = sanitize(body_with_embedding(article), tags: %w(blockquote img a script), attributes: %w(class href src charset async))
+    safe_html = sanitize(body_with_embedding(article), tags: %w(blockquote img a script), attributes: %w(class href src charset async data-instgrm-permalink))
 
     content_tag :span, safe_html, style: 'white-space: pre-wrap;'
   end
@@ -11,6 +11,30 @@ module ArticlesHelper
     body_html = twitter_urls.inject(body_html) do |html, url|
       html.gsub(url, twitter_embed(url))
     end
+
+    instagram_urls = body_html.scan(/https:\/\/www.instagram.com\/p\/.+/)
+    body_html = instagram_urls.inject(body_html) do |html, url|
+      html.gsub(url, instagram_embed(url))
+    end
+
+    instagram_reel_urls = body_html.scan(/https:\/\/www.instagram.com\/reel\/.+/)
+    body_html = instagram_reel_urls.inject(body_html) do |html, url|
+      html.gsub(url, instagram_reel_embed(url))
+    end
+  end
+
+  def instagram_reel_embed(url)
+    embed_body = content_tag :blockquote, class: 'instagram-media', 'data-instgrm-permalink': url do
+      content_tag(:a, '', href: url)
+    end
+    embed_body + content_tag(:script, '', src: 'https://www.instagram.com/embed.js', charset: 'utf-8', async: true)
+  end
+
+  def instagram_embed(url)
+    embed_body = content_tag :blockquote, class: 'instagram-media', 'data-instgrm-permalink': url do
+      content_tag(:a, '', href: url)
+    end
+    embed_body + content_tag(:script, '', src: 'https://www.instagram.com/embed.js', charset: 'utf-8', async: true)
   end
 
   def twitter_embed(url)
