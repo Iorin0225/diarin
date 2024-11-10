1ecd592a8ca67491fcd22c7fc978c82605c17f73
Iori OSADA <iori.osada@gmail.com>
2024-11-10 09:41:39 +0900

    feat: show img and a tag on body, and add back button

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,5 +1,6 @@
 module ArticlesHelper
   def body_with_pre_wrap(article)
-    content_tag :span, article.body, style: 'white-space: pre-wrap;'
+    safe_html = sanitize(article.body, tags: %w(img a), attributes: %w(href src))
+    content_tag :span, safe_html, style: 'white-space: pre-wrap;'
   end
 end
