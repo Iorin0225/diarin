134d8991de63f8ab4657e1bf4aafb40c2cacb03d
Iori OSADA <iori.osada@gmail.com>
2024-11-10 11:27:23 +0900

    feat: add Next and Prev buttons.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,16 +1,25 @@
 class Article < ApplicationRecord
   belongs_to :book
 
-  scope :published, -> { where('published_at < ?', Time.zone.now) }
+  # scope :published, -> { where('published_at < ?', Time.zone.now) }
+  scope :published, -> { where(status: :publish) }
 
   scope :with_year, -> (year) {
-    start_date = Date.new(year.to_i, 1, 1).beginning_of_year
+    start_date = Time.zone.local(year.to_i, 1, 1).beginning_of_year
     end_date = start_date.end_of_year
     where(published_at: start_date..end_date)
   }
   scope :with_year_month, -> (year, month) {
-    start_date = Date.new(year.to_i, month.to_i, 1).beginning_of_month
+    start_date = Time.zone.local(year.to_i, month.to_i, 1).beginning_of_month
     end_date = start_date.end_of_month
     where(published_at: start_date..end_date)
   }
+
+  def prev_article
+    @prev_article ||= book.articles.published.where('published_at < ?', published_at).order(published_at: :desc).first
+  end
+
+  def next_article
+    @next_article ||= book.articles.published.where('published_at > ?', published_at).order(published_at: :asc).first
+  end
 end
