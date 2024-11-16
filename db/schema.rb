26018b528ba8172dcb00d79811bf6673fc69fae7
Iori OSADA <iori.osada@gmail.com>
2024-11-17 08:26:42 +0900

    chore: upgrade to rails 8.0 without rc

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_11_10_004305) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_16_232325) do
   create_table "articles", force: :cascade do |t|
     t.string "title", null: false
     t.text "body", null: false
