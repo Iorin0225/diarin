a78031ea5998b245587acec5341ffdcb2fec3eb2
Iori OSADA <iori.osada@gmail.com>
2024-12-01 11:45:17 +0900

    migrate: add display_name to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_01_003750) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_01_022502) do
   create_table "articles", force: :cascade do |t|
     t.string "title", null: false
     t.text "body", null: false
@@ -48,6 +48,7 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_01_003750) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "display_name", null: false
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
 
