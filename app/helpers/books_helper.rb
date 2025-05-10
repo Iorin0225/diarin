85e2e0b1fd0f72816c889817ab1a7595abf68eb7
Iori OSADA <iori.osada@gmail.com>
2025-05-10 11:50:19 +0900

    feat: add updated date on books list

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,2 +1,10 @@
 module BooksHelper
+  def book_updated_label(book)
+    book_updated_at = book.articles.published.last&.published_at
+
+    return '' if book_updated_at.nil?
+
+    return "#{date_only(book_updated_at)} 更新"
+
+  end
 end
