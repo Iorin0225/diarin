9d9454ae224ef5727bb502a2103ccd47c5da0a90
Iori OSADA <iori.osada@gmail.com>
2025-05-31 08:27:11 +0900

    fix: updated label should get the latest published_at

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,6 +1,6 @@
 module BooksHelper
   def book_updated_label(book)
-    book_updated_at = book.articles.published.last&.published_at
+    book_updated_at = book.articles.published.order(published_at: :desc).first&.published_at
 
     return '' if book_updated_at.nil?
 
